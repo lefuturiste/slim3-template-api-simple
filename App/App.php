31b34f652ec13c3c903c3794a4be15a3aabef74e
Matthieu Bessat <spamfree@matthieubessat.fr>
2018-07-08 00:26:48 +0200

    snd

--- App/App.php
+++ App/App.php
@@ -5,21 +5,10 @@ use DI\ContainerBuilder;
 
 class App extends \DI\Bridge\Slim\App
 {
-	protected $bundles = [];
-	protected $hasBundles = false;
-	protected $twigPaths= ['../App/views'];
-
 	protected function configureContainer(ContainerBuilder $builder)
 	{
 		$builder->addDefinitions(__DIR__ . '/config/app.php');
 		$builder->addDefinitions(__DIR__ . '/config/api.php');
-		$builder->addDefinitions(__DIR__ . '/config/database.php');
 		$builder->addDefinitions(__DIR__ . '/config/containers.php');
-	}
-
-	public function getBundles(){
-		return [
-			//\App\AdminBundle\AdminBundle::class,
-		];
 	}
 }
--- App/Controllers/PagesController.php
+++ App/Controllers/PagesController.php
@@ -10,12 +10,6 @@ class PagesController extends Controller
 {
 	public function getHome(ServerRequestInterface $request, ResponseInterface $response)
 	{
-		$this->container->get(Logger::class)->info('Hello');
-
-//		return a 404 http error
-//		throw new NotFoundException($request, $response);
-
-//		return json
 		return $response->withJson([
 			'This is the main page of the API!',
 			'Welcome in your application!'
